16b0fbb21ae04bbd0fdf184b4112c1100dbd4983
javierruvalcaba <javierruvalcaba@drata.com>
2022-11-29 13:19:42 -0600

    core(serivices) add services folder to manage db interaction and data manipulation

---
+++ services/ExampleService.js
@@ -0,0 +1,13 @@
+export class ExamplePatientService {
+    async getAllPatients() {
+        throw new Error('Method not implemented')
+    }
+
+    async getPatient() {
+        throw new Error('Method not implemented')
+    }
+
+    async addPatient() {
+        throw new Error('Method not implemented')
+    }
+}
